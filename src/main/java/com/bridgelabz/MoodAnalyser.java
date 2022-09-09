9e00f3e112cd1346c8e624f24d68b20a528076c0
amolmane7471 <maneamol3233@gmail.com>
2022-09-09 19:13:34 +0530

    [Amol] : refactor the code to take massage in constructor

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,6 +1,13 @@
 package com.bridgelabz;
 public class MoodAnalyser {
+    private String message;
 
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
     public String analyseMood(String message) {
         if (message.contains("Sad"))
             return "SAD";
